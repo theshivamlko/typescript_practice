3783f59dedcae8b60f0cc9b31d8b611993adfe8c
Shivam Srivastava <shivamsrivastava.9@gmail.com>
2024-11-22 15:41:56 +0530

    object n arrays

--- src/app.js
+++ src/app.js
@@ -1,1 +1,61 @@
-console.log("Hello");
+console.log("Hello World");
+// working with numbers
+function add(n1, n2) {
+    return n1 + n2;
+}
+var number1 = 5;
+var number2 = 2.8;
+var number3 = '5';
+var number3_1 = '5'; // once set string is set for other var update
+console.log(add(number1, number2));
+console.log(typeof number1);
+console.log(typeof number2);
+console.log(typeof number3);
+var number4 = '5';
+var number5 = true;
+console.log(number4);
+console.log(number5);
+console.log(typeof number5);
+console.log(typeof number5);
+// Objects
+var person = {
+    name: "Shivam",
+    age: 30
+};
+var p1 = {
+    name: "Shivam",
+    age: 30
+};
+console.log(person);
+console.log(typeof person); // type object
+console.log(person.name); // type object
+var person2 = {
+    name: "Shivam",
+    age: 30
+};
+console.log(person2);
+console.log(typeof person2);
+var person3 = {
+    name: "Shivam",
+    age: 30
+};
+console.log(person3);
+console.log(typeof person3);
+console.log("Arrays ==========");
+var person6 = {
+    name: "Shivam",
+    age: 30,
+    hobbies: ['Sports', 'Cooking'],
+    ranks: ['A+', 'S', 1]
+};
+console.log(person6);
+console.log(typeof person6.hobbies);
+console.log(typeof person6.ranks);
+var favorite;
+favorite = ["A", "B"];
+console.log(favorite);
+console.log(favorite.length);
+for (var _i = 0, favorite_1 = favorite; _i < favorite_1.length; _i++) {
+    var i = favorite_1[_i];
+    console.log(i.toLowerCase());
+}
--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,78 @@
-console.log("Hello")
+console.log("Hello World")
+
+// working with numbers
+function add(n1: number, n2: number) {
+    return n1 + n2;
+}
+
+const number1 = 5;
+const number2 = 2.8;
+let number3: string = '5';
+let number3_1 = '5'; // once set string is set for other var update
+
+console.log(add(number1, number2))
+
+console.log(typeof number1)
+console.log(typeof number2)
+console.log(typeof number3)
+
+const number4 = '5';
+const number5: boolean = true;
+console.log(number4)
+console.log(number5)
+console.log(typeof number5)
+console.log(typeof number5)
+
+// Objects
+const person = {
+    name: "Shivam",
+    age: 30
+}
+const p1: object = {
+    name: "Shivam",
+    age: 30
+}
+console.log(person);
+console.log(typeof person); // type object
+console.log(person.name); // type object
+
+const person2: {} = {
+    name: "Shivam",
+    age: 30
+}
+console.log(person2);
+console.log(typeof person2);
+
+
+const person3: {
+    name: string,
+    age: number
+} = {
+    name: "Shivam",
+    age: 30
+}
+console.log(person3);
+console.log(typeof person3);
+
+
+// Arrays
+console.log("Arrays ==========");
+
+const person6 = {
+    name: "Shivam",
+    age: 30,
+    hobbies:['Sports','Cooking'],
+    ranks:['A+','S',1]
+}
+console.log(person6);
+console.log(typeof person6.hobbies);
+console.log(typeof person6.ranks);
+
+let favorite:string[];
+favorite=["A","B"]
+console.log(favorite);
+console.log(favorite.length);
+
+for(const i of favorite)
+    console.log(i.toLowerCase());
 
